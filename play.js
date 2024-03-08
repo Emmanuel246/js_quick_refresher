8ed39796041c6e4622b62d45554b82fa12c62751
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-03-08 22:55:29 +0100

    arrow function

--- play.js
+++ play.js
@@ -2,11 +2,15 @@ const name = "Emmanuel";
 const age = 20;
 const hasHobbies = true;
 
-function summarizeUser(userName, userAge, UserHasHobby) {
+const summarizeUser = (userName, userAge, UserHasHobby) => {
     return (
         "Name is " + userName
         + ", age is " + userAge + " and the user has hobbies: " + UserHasHobby
     );
 }
+const add = (a, b) => a + b;
+console.log(add(2,4))
 
+const random = () => 1 + 4;
+console.log(random(1));
 console.log(summarizeUser(name, age, hasHobbies))
