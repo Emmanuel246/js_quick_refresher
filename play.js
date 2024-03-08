6be9e1802bf7e3df1844155fb5c935720c38b8a3
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-03-08 23:54:49 +0100

    spread and rest operator

--- play.js
+++ play.js
@@ -8,13 +8,25 @@ const person = {
 
 // person.greet();
 
-
+const copiedPerson = {...person};
+console.log(copiedPerson);
 const hobbies = ['Sports', 'Cooking'];
 
-for (let hobby of hobbies) {
-    console.log(hobby)
+// for (let hobby of hobbies) {
+//     console.log(hobby)
 
-}
+// }
 
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby))
-console.log(hobbies)
+
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+const toArray = (...args) => {
+    return args;
+
+};
+
+console.log(toArray(1,2,3,2,2,1,))
+
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby))
+// console.log(hobbies)
